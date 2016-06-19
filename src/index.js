2eaf317ee88e77cced4edee7b43bf35c4024916b
Yeiber Cano <yeibercano@gmail.com>
2016-06-19 15:14:23 -0700

    css loader and style workking with webpack and react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // require("./test.js"); // es5 syntax
 
 //TESTING ES6 WITH BABEL
-import greetings from './test.js';
-document.write(greetings("Yeiber", "enjoys coding on Sundays"));
+// import greetings from './test.js';
+// document.write(greetings("Yeiber", "enjoys coding on Sundays"));
+
+import styles from './index.css'
+
+let item = `
+  <div class="item">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
+  </div>
+`
+
+document.write(item);
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
         test: /\.js/,
         loader: 'babel',
         include: __dirname + '/src',
-      }
+      },
+      {
+        test: /\.css/,
+        // if you use an array, loaders should be plural
+        loaders: ['style', 'css'],
+        //otherwise
+        // loader: 'style!css', 
+        include: __dirname + '/src',
+      },
     ]
   }
 };
