2e776a5c7a85c1c5394d90ccdd991bb7a0ee71ed
Yeiber Cano <yeibercano@gmail.com>
2016-06-19 14:59:14 -0700

    babel es6 and webpack working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-require("./test.js");
+// require("./test.js"); // es5 syntax
+
+//TESTING ES6 WITH BABEL
+import greetings from './test.js';
+document.write(greetings("Yeiber", "enjoys coding on Sundays"));
--- src/test.js
+++ src/test.js
@@ -1,1 +1,8 @@
 alert("Webpack and React working");
+
+//TESTING BABEL - using const, export, template, arrow f, 
+const greetings = (person, text) => {
+  return `${person} ${text}. This is a templae literal example with ES6`;
+}
+
+export default greetings;
