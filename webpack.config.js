c2e9d666f26efffe74d8bcefbaf46c1d62a626ad
Yeiber Cano <yeibercano@gmail.com>
2016-06-19 15:24:07 -0700

    change css and style loaders for ExtractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
 module.exports = {
   entry: './src',
   output: {
@@ -11,14 +14,22 @@ module.exports = {
         loader: 'babel',
         include: __dirname + '/src',
       },
+      //this approach would add a style tag to your markup
+      // {
+      //   test: /\.css/,
+      //   // if you use an array, loaders should be plural
+      //   loaders: ['style', 'css'],
+      //   //otherwise
+      //   // loader: 'style!css', 
+      //   include: __dirname + '/src',
+      // },
       {
         test: /\.css/,
-        // if you use an array, loaders should be plural
-        loaders: ['style', 'css'],
-        //otherwise
-        // loader: 'style!css', 
-        include: __dirname + '/src',
+        loader: ExtractTextPlugin.extract('css'),
       },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 };
